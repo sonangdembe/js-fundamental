6f0e52dc231aac865145de99e6761543b465161e
sona angdembe <asonaxaviersm1@gmail.com>
2023-04-02 12:51:39 +0545

    this is full developed output

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ submitButton.addEventListener("click",function(e){
     let signupForm = document.forms["signupForm"];
     //console.log(signupForm);
   let fristNameInput =signupForm.fristName;
-  let lastNameInput = signupForm.lastName
+  let lastNameInput = signupForm.lastName;
+  let emailAddressInput = signupForm.emailAddress;
+  let passwordInput= signupForm.password;
  // console.log(fristNameInput.value);
 
 if(fristNameInput.value == ""){
@@ -18,9 +20,23 @@ if(lastNameInput.value == ""){
   errors.lastName = 'lastName should not be empty';
   
 }
-console.log(errors);
-for (let error in errors){
+if(emailAddressInput.value == ""){
+  errors.email = 'Email should not be empty';
+}
+if(passwordInput.value <=5){
+  errors.password = 'password should not be empty';
+}
 
+console.log(errors);
+for (let error in errors) {  
+let lastNameGroup = document.getElementById(`${error}Group`);
+let lastNameMessage = document.querySelector(`#${error}Group .message`);
+lastNameGroup.classList.add("error");
+lastNameMessage.textContent =errors[error];
+
+}
+if(Object.keys(errors).length == 0 ){
+signupForm.submit();
 }
 });
 
@@ -30,7 +46,3 @@ for (let error in errors){
 // fristNameGroup.classList.add("error");
 // fristNameMessage.textContent ="FristName should not be empty";
 
-// let lastNameGroup = document.getElementById('lastNameGroup');
-//   let lastNameMessage = document.querySelector('#lastNameGroup .message');
-//   lastNameGroup.classList.add("error");
-//   lastNameMessage.textContent ="lastName should not be empty";
