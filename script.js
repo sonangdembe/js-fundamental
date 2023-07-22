e21017c4fe569580bdd0025daabada09a270e8cd
sona <103208814+s1o2n3a4@users.noreply.github.com>
2023-07-22 16:39:58 +0545

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const refreshBtn = document.querySelector(".refresh-word");
 const checkBtn = document.querySelector(".check-word");
 
 let correctWord, timer;
+let score = 0; // Initialize score outside the initGame function
+let usedWords = new Set(); // Keep track of used words
 
 const initTimer = maxTime => {
     clearInterval(timer);
@@ -15,15 +17,33 @@ const initTimer = maxTime => {
             timeText.innerText = maxTime;
         } else {
             clearInterval(timer);
-            alert(`Time's up! ${correctWord.toUpperCase()} was the correct word`);
+            if (inputField.value === "") {
+                // If no input is provided until time ends, reset the score to zero
+                score = 0;
+                alert("Time's up! You didn't enter any word. Your score has been reset to 0.");
+            } else {
+                const finalScore = score;
+                alert(`Time's up! Your final score: ${finalScore}`);
+            }
             initGame();
         }
     }, 1000);
 };
 
+const getRandomWord = () => {
+    // Get a random word that has not been used in the current round
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    while (usedWords.has(randomObj.word)) {
+        randomObj = words[Math.floor(Math.random() * words.length)];
+    }
+    usedWords.add(randomObj.word);
+    return randomObj;
+};
+
 const initGame = () => {
     initTimer(30);
-    let randomObj = words[Math.floor(Math.random() * words.length)];
+    usedWords.clear(); // Clear the usedWords set for a new round
+    const randomObj = getRandomWord();
     let wordArray = randomObj.word.split("");
     for (let i = wordArray.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
@@ -39,8 +59,13 @@ const initGame = () => {
 const checkWord = () => {
     let userWord = inputField.value.toLowerCase();
     if (!userWord) return alert("Please enter the word to check!");
-    if (userWord !== correctWord) return alert(`Oops! ${userWord} is not the correct word`);
-    alert(`Congrats! ${correctWord.toUpperCase()} is the correct word`);
+    if (userWord !== correctWord) {
+        alert(`Oops! ${userWord} is not the correct word. Your score: ${score}`);
+        score = 0;
+    } else {
+        score++;
+        alert(`Congrats! ${correctWord.toUpperCase()} is the correct word. Your score: ${score}`);
+    }
     initGame();
 };
 
