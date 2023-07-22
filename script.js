f4ab5c0ac6859f400f196a44c5a21b5e6891633e
sona <103208814+s1o2n3a4@users.noreply.github.com>
2023-07-22 15:39:16 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+const wordText = document.querySelector(".word");
+const hintText = document.querySelector(".hint span");
+const timeText = document.querySelector(".time b");
+const inputField = document.querySelector("input");
+const refreshBtn = document.querySelector(".refresh-word");
+const checkBtn = document.querySelector(".check-word");
+
+let correctWord, timer;
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if (maxTime > 0) {
+            maxTime--;
+            timeText.innerText = maxTime;
+        } else {
+            clearInterval(timer);
+            alert(`Time's up! ${correctWord.toUpperCase()} was the correct word`);
+            initGame();
+        }
+    }, 1000);
+};
+
+const initGame = () => {
+    initTimer(30);
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    wordText.innerText = wordArray.join("");
+    hintText.innerText = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();
+    inputField.value = "";
+    inputField.setAttribute("maxlength", correctWord.length);
+};
+
+const checkWord = () => {
+    let userWord = inputField.value.toLowerCase();
+    if (!userWord) return alert("Please enter the word to check!");
+    if (userWord !== correctWord) return alert(`Oops! ${userWord} is not the correct word`);
+    alert(`Congrats! ${correctWord.toUpperCase()} is the correct word`);
+    initGame();
+};
+
+const handleEnterKey = event => {
+    if (event.key === "Enter") {
+        checkWord();
+    }
+};
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
+inputField.addEventListener("keypress", handleEnterKey);
+
+initGame();
